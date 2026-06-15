7eeba0bae41b3a76a9c12a14f9f09dc3c919c17f
Shaked Falah <89452029+ShakedFalah@users.noreply.github.com>
2026-06-15 12:41:40 +0300

    added bgm

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -2,7 +2,17 @@ using UnityEngine;
 
 public class AudioManager : Singleton<AudioManager>
 {
-    public string sfxTag = "SFXSource";
+    [Header("BGM")]
+    [SerializeField] private AudioSource bgmAudioSource;
+    [SerializeField] private AudioClip bgmClip;
+    [Header("SFX")]
+    [SerializeField] private string sfxTag = "SFXSource";
+
+    private void Start()
+    {
+        PlayMusic();
+    }
+
     public void PlaySound(AudioClip audioClip, Transform parent = null)
     {
         PlaySFXPool sfxPlayer = TaggedObjectPooler.Instance.GetPooledObjectWithAutoReturn(sfxTag).GetComponent<PlaySFXPool>();
@@ -11,5 +21,17 @@ public class AudioManager : Singleton<AudioManager>
             sfxPlayer.transform.SetParent(transform);
         }
         sfxPlayer.PlaySound(audioClip, sfxPlayer.TriggerReturn);
+    }
+
+    public void PlayMusic()
+    {
+        if (bgmAudioSource == null || bgmClip == null)
+        {
+            Debug.LogWarning("No audio source or clip for bgm");
+            return;
+        }
+
+        bgmAudioSource.clip = bgmClip;
+        bgmAudioSource.Play();
     }
 }
